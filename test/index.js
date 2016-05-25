77a1289dba79d59bd3cf920d5df4373f5d77a432
creeperyang <yangcreeper@hotmail.com>
2016-05-25 15:07:32 +0800

    add complex doctype test

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ const HtmlParser = require('../src')
 const Node = require('../src/node')
 
 describe('API', function() {
-    it('should parse html to nodes tree', function() {
+    it('should parse html to nodes tree correctly', function() {
         let html = fs.readFileSync(path.resolve(__dirname, 'textures/simple.html'))
         let parser = new HtmlParser()
         let tree = parser.parse(html.toString())
@@ -26,6 +26,25 @@ describe('API', function() {
                 class: 'html-ok'
             },
             className: 'html-ok'
+        })
+    })
+    it('should parse complex doctype correctly', function() {
+        let html = fs.readFileSync(path.resolve(__dirname, 'textures/doctype.html'))
+        let parser = new HtmlParser()
+        let tree = parser.parse(html.toString())
+        tree.should.have.properties({
+            tagName: 'document',
+            nodeType: 9,
+            parentNode: undefined
+        })
+        tree.should.have.property('childNodes').with.lengthOf(1)
+        tree.childNodes[0].should.be.instanceof(Node.DoctypeNode).and.have.properties({
+            tagName: 'doctype',
+            nodeType: 10,
+            parentNode: tree,
+            publicId: '-//W3C//DTD XHTML 1.0 Transitional//EN',
+			systemId: 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd',
+            name: 'html'
         })
     })
 })
